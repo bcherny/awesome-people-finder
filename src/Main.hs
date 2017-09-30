49b3d3be906aa46b7d923c57ebe902a69160cebd
Boris Cherny <boris@performancejs.com>
2017-09-29 20:55:46 -0700

    fetch repos - done

--- src/Main.hs
+++ src/Main.hs
@@ -6,13 +6,38 @@ import Data.Aeson (encode, object, (.=))
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Network.HTTP.Types.Status (statusCode)
 
 main :: IO ()
 main = do
-  manager <- newManager tlsManagerSettings
-  initialRequest <- parseRequest "https://api.github.com/search/repositories"
-  let request = setQueryString [("q", Just "language:typescript")] initialRequest {
-    requestHeaders = [("User-Agent", "Haskell")]
-  }
-  response <- httpLbs request manager
-  L8.putStrLn $ responseBody response
+  response <- getRepos
+  logResponse (processResponse response)
+
+getRepos :: IO (Response L8.ByteString)
+getRepos = do
+  getRepos' 1
+  where
+    getRepos' :: Int -> IO (Response L8.ByteString)
+    getRepos' page = do
+      manager <- newManager tlsManagerSettings
+      initialRequest <- parseRequest "https://api.github.com/search/repositories"
+      let request = initialRequest {
+        requestHeaders = [("User-Agent", "Haskell")],
+        queryString = "q=language:typescript&sort=stars"
+      }
+      httpLbs request manager
+
+processResponse :: Response L8.ByteString -> Maybe L8.ByteString
+processResponse response = case isFailure response of
+  True -> Nothing
+  False ->
+    Just $ responseBody response
+
+logResponse :: Maybe L8.ByteString -> IO ()
+logResponse Nothing = L8.putStrLn "Error"
+logResponse (Just a) = L8.putStrLn a
+
+isFailure :: Response L8.ByteString -> Bool
+isFailure response
+  | (statusCode (responseStatus response)) == 200 = False
+  | otherwise = True
